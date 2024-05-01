813c4104055f878bec303d12a63dff01bc899121
Ayden Cook <cookayden2003@gmail.com>
2024-05-01 02:33:54 -0400

    band plotting

--- tight_binding.py
+++ tight_binding.py
@@ -17,17 +17,32 @@ def tb_from_tiling(tiling, onsite_en=0.0, hop_amp=-1.0):
         tb_model.set_hop(hop_amp[i], pair[0], pair[1], ind_R=[0, 0])
     return tb_model
 
+
+def plot_band_structure(tb_model, mesh_size=(50, 50), title="Band Structure", legend=False):
+    k_mesh = tb_model.k_uniform_mesh(mesh_size)
+    eigen_vals = tb_model.solve_all(k_mesh)
+    fig = plt.figure(title)
+    ax = fig.add_subplot()
+    plt.title(title)
+    for i, band in enumerate(eigen_vals):
+        ax.plot(band, label="Band {}".format(i+1))
+    ax.set_xlabel("k-point index")
+    ax.set_ylabel("Energy")
+    plt.legend() if legend else None
+    plt.show()
+
 ##############################################################################################
 
 
 if __name__ == "__main__":
     from uniform_tilings_1 import UNITS
     from tiling import Tiling
-    import matplotlib.pyplot as pyplot
+    import matplotlib.pyplot as plt
     for unit in UNITS:
         tiling = Tiling().add_unit_pattern(unit)
         model = tb_from_tiling(tiling)
-        tiling.render_unit(pyplot)
-        model.visualize(0, dir_second=1)
+        # tiling.render_unit(pyplot)
+        # model.visualize(0, dir_second=1)
+        plot_band_structure(model, title=unit.__name__+" Band Structure")
 
 ##############################################################################################
